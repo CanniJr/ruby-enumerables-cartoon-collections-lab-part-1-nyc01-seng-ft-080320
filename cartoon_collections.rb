dc673c299cb10d8526824a01dfcbd04b362e8e4c
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 03:36:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
   array.each do |a|
-  # Print a custom greeting for each element
   puts "Hello #{a}!"
 end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
-  #
+  array.each_with_index do |b|
+    
   # Print a numbered list of each element
+  
 end
