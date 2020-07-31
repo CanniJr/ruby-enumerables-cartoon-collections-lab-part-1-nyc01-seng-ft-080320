966e2351c95754a704e6d9386ea0e049bfc94a6b
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 03:23:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   array.each do |a|
   # Print a custom greeting for each element
-  puts "#{a}!"
+  puts a
 end
 end
 
